0f1e5fd07e0fd767333aeb8c456bf98dec15f8ad
Mohamed <mohamed.xyz@hotmail.com>
2019-04-25 22:51:08 +0200

    style: Constants naming and position

--- src/view/game/stats/HealthBars.java
+++ src/view/game/stats/HealthBars.java
@@ -11,22 +11,21 @@ import view.GameViewManager;
 import java.util.ArrayList;
 
 public class HealthBars extends VBox {
+    private static final int HP_HEIGHT = 20;
+    private static final int SHIELD_HEIGHT = 7;
+    private static final int BARWIDTH = 300;
+    private static final Color HP_COLOR = Color.DARKRED;
+    private static final Color SHIELD_COLOR = Color.LIGHTBLUE;
 
     private ArrayList <StatBar> statRectangles = new ArrayList<>(); //0 for HP, 1 for Shield
-
-    private static final int HPHEIGHT = 20;
-    private static final int SHIELDHEIGHT = 7;
-    private static final int BARWIDTH = 300;
-    private static final Color HPCOLOR = Color.DARKRED;
-    private static final Color SHIELDCOLOR = Color.LIGHTBLUE;
 
     public HealthBars() {
         VBox HPVBox = new VBox();
         HPVBox.setSpacing(5);
         HPVBox.setPadding(new Insets(5, 10, 0, 5));
 
-        StackPane stackHP = getStackPane(HPHEIGHT,HPCOLOR,false,Player.getMaxHp());
-        StackPane stackShield = getStackPane(SHIELDHEIGHT,SHIELDCOLOR,true,Player.getMaxShield());
+        StackPane stackHP = getStackPane(HP_HEIGHT, HP_COLOR,false,Player.getMaxHp());
+        StackPane stackShield = getStackPane(SHIELD_HEIGHT, SHIELD_COLOR,true,Player.getMaxShield());
 
         HPVBox.getChildren().addAll(stackHP, stackShield);
         GameViewManager.addGameObjectTOScene(HPVBox);
