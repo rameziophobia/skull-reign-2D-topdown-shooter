c0e378daec68a2940183ff6c4592e4592a28b5ff
Mohamed <mohamed.xyz@hotmail.com>
2019-05-11 17:12:05 +0200

    feat: AudioFile takes the name of the file instead of the path

--- src/controller/audiomanager/AudioFile.java
+++ src/controller/audiomanager/AudioFile.java
@@ -3,31 +3,45 @@ package controller.audiomanager;
 import view.Main;
 
 public enum AudioFile {
-    FOOTSTEPS(Constants.PATH_RESOURCES_AUDIO_SFX  + "336598__inspectorj__footsteps-concrete-a.wav", MixerType.SFX, 0.85),
-    BURST(Constants.PATH_RESOURCES_AUDIO_SFX_PROJECTILES  + "burst.mp3", MixerType.SFX, 0.85),
-    S1(Constants.PATH_RESOURCES_AUDIO_SFX_PROJECTILES  + "s1.mp3", MixerType.SFX, 0.85),
-    S2(Constants.PATH_RESOURCES_AUDIO_SFX_PROJECTILES  + "s2.mp3", MixerType.SFX, 0.85),
-    S3(Constants.PATH_RESOURCES_AUDIO_SFX_PROJECTILES  + "s3.mp3", MixerType.SFX, 0.85),
-    MACHINE(Constants.PATH_RESOURCES_AUDIO_SFX_PROJECTILES  + "machine.mp3", MixerType.SFX, 0.85),
-    LASER(Constants.PATH_RESOURCES_AUDIO_SFX_PROJECTILES  + "laser.wav", MixerType.SFX, 0.85),
-    WOOSH(Constants.PATH_RESOURCES_AUDIO_SFX_PROJECTILES  + "woosh.mp3", MixerType.SFX, 0.85),
-    ELECTRIC1(Constants.PATH_RESOURCES_AUDIO_SFX_PROJECTILES  + "electric1.mp3", MixerType.SFX, 0.85);
+    FOOTSTEPS("336598__inspectorj__footsteps-concrete-a.wav", MixerType.SFX, 0.85),
+    BURST(Constants.FOLDER_PROJECTILE + "burst.mp3", MixerType.SFX, 0.85),
+    S1(Constants.FOLDER_PROJECTILE + "s1.mp3", MixerType.SFX, 0.85),
+    S2(Constants.FOLDER_PROJECTILE + "s2.mp3", MixerType.SFX, 0.85),
+    S3(Constants.FOLDER_PROJECTILE + "s3.mp3", MixerType.SFX, 0.85),
+    MACHINE(Constants.FOLDER_PROJECTILE + "machine.mp3", MixerType.SFX, 0.85),
+    LASER(Constants.FOLDER_PROJECTILE + "laser.wav", MixerType.SFX, 0.85),
+    WOOSH(Constants.FOLDER_PROJECTILE + "woosh.mp3", MixerType.SFX, 0.85),
+    ELECTRIC1(Constants.FOLDER_PROJECTILE + "electric1.mp3", MixerType.SFX, 0.85);
 
     private final String path;
     private final MixerType mixer;
     private final double volume;
     private final int cycleCount;
 
-    AudioFile(String path, MixerType mixer) {
-        this(path, mixer, 1, 1);
+    AudioFile(String name, MixerType mixer) {
+        this(name, mixer, 1, 1);
     }
 
-    AudioFile(String path, MixerType mixer, double volume) {
-        this(path, mixer, volume, 1);
+    AudioFile(String name, MixerType mixer, double volume) {
+        this(name, mixer, volume, 1);
     }
 
-    AudioFile(String path, MixerType mixer, double volume, int cycleCount) {
-        this.path = path;
+    AudioFile(String name, MixerType mixer, double volume, int cycleCount) {
+        final String path;
+        switch (mixer) {
+            case SFX:
+                path = Constants.PATH_RESOURCES_AUDIO_SFX;
+                break;
+            case MUSIC:
+                path = Constants.PATH_RESOURCES_AUDIO_MUSIC;
+                break;
+            case AMBIENT:
+                path = Constants.PATH_RESOURCES_AUDIO_AMBIENT;
+                break;
+            default:
+                throw new UnsupportedOperationException();
+        }
+        this.path = path + name;
         this.mixer = mixer;
         this.volume = volume;
         this.cycleCount = cycleCount;
@@ -52,6 +66,9 @@ public enum AudioFile {
     private static class Constants {
         private static final String PATH_RESOURCES_AUDIO = Main.PATH_RESOURCES + "audio/";
         private static final String PATH_RESOURCES_AUDIO_SFX = PATH_RESOURCES_AUDIO + "sfx/";
-        private static final String PATH_RESOURCES_AUDIO_SFX_PROJECTILES = PATH_RESOURCES_AUDIO_SFX + "projectiles/";
+        private static final String PATH_RESOURCES_AUDIO_MUSIC = PATH_RESOURCES_AUDIO + "music/";
+        private static final String PATH_RESOURCES_AUDIO_AMBIENT = PATH_RESOURCES_AUDIO + "ambient/";
+
+        private static final String FOLDER_PROJECTILE = "projectiles/";
     }
 }
