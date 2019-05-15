fbd765f0f7d144abf34bca07c198efb268d68e72
Mohamed <mohamed.xyz@hotmail.com>
2019-05-15 04:53:02 +0200

    fix: player name textfield is no longer editable while hidden

--- src/view/menu/mainmenu/menus/CustomizationMenu.java
+++ src/view/menu/mainmenu/menus/CustomizationMenu.java
@@ -8,21 +8,33 @@ import model.ui.menu.MenuButtonTransition;
 import view.menu.mainmenu.MenuScene;
 
 public class CustomizationMenu extends Menu {
+    private final TextField txt_playerName;
+
     public CustomizationMenu(MenuScene menuScene) {
         super(menuScene);
 
-        TextField txt_playerName = new TextField();
+        txt_playerName = new TextField();
         txt_playerName.setPromptText("Enter Name Here...");
+        txt_playerName.setFont(Font.font(20));
+
         txt_playerName.setMaxWidth(350);
         txt_playerName.setPrefHeight(50);
         txt_playerName.setPadding(new Insets(0, 0, 0, 5));
-        txt_playerName.setFont(Font.font(20));
+
+        txt_playerName.setFocusTraversable(false);
+        txt_playerName.setDisable(true);
 
         addNodeAll(
                 MenuScene.createMenuTitle("Enter Player Name"),
                 txt_playerName,
-                new MenuButtonTransition("Next", this, Menus.NEW_GAME),
+                new MenuButtonTransition("Next", this, Menus.NEW_GAME, () -> txt_playerName.setDisable(true)),
                 new MenuButtonTransition("Back", this, Menus.MAIN, menuScene::closeDoor)
         );
+    }
+
+    @Override
+    public void fadeIn() {
+        super.fadeIn();
+        txt_playerName.setDisable(false);
     }
 }
