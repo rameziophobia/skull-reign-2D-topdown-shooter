08b77324d516398f5491bc519cae2ef4320f4494
Mohamed <mohamed.xyz@hotmail.com>
2019-05-19 01:03:02 +0200

    stuff

--- src/controller/audiomanager/AudioFile.java
+++ src/controller/audiomanager/AudioFile.java
@@ -18,7 +18,7 @@ public enum AudioFile {
     GAME_MUSIC_BASIC(Constants.FOLDER_GAME+"backgroundmusic.mp3",MixerType.MUSIC,0.4,AudioClip.INDEFINITE),
     FIRE("fire.mp3", MixerType.SFX, 0.05, 1),//todo
     FIREBALL("334234__liamg-sfx__fireball-cast-1.wav", MixerType.SFX, 0.9),
-    HURT("SFX_Hurt10.wav", MixerType.SFX, 1),
+    HURT("SFX_Hurt10.wav", MixerType.SFX, 0.2),
     PLAYER_DEATH(Constants.FOLDER_PLAYER+"playerdeath.wav",MixerType.SFX,0.6,1),
     PLAYER_POWERUP(Constants.FOLDER_PLAYER+"powerup.wav",MixerType.SFX,0.6,1),
     ENEMY_DEATH("enemydeath.wav",MixerType.SFX,0.2,1),
